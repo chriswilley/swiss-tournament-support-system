7894c47b7e509992b1d0f6e2430857a7ed422799
Chris Willey <cwilley@gmail.com>
2015-10-22 13:56:24 +0000

    Fixed some typos and added more comments to tournament.py.

--- tournament/tournament.py
+++ tournament/tournament.py
@@ -22,11 +22,13 @@ def get_cursor():
     conn = connect()
     cur = conn.cursor()
     try:
+        # the yield function here will provide the cursor if there are no
+        # errors, so that it can be "return"ed to the calling function
         yield cur
-        conn.commit()
+        conn.commit()  # commit any database changes, such as an INSERT
         return
     except:
-        raise
+        raise  # pass any error back to the calling function as is
     finally:
         cur.close()
         conn.close()
@@ -41,9 +43,13 @@ def deleteMatches(tournament=0):
     """
     sql = 'DELETE FROM matches'
     data = ('',)
+
     if (type(tournament) is int and tournament != 0):
+        # if function is called with a tournament specified, only delete the
+        # matches in that tournament
         sql += ' WHERE tournament_id = %s'
-        data = ('tournament')
+        data = (tournament,)  # prevents SQL injection
+
     with get_cursor() as cursor:
         cursor.execute(sql, data)
 
@@ -67,11 +73,16 @@ def createTournament(t_name):
     Args:
       name: the name of the tournament (need not be unique).
     """
+    # use "RETURNING id" to make sure to return the tournament ID so
+    # we can pass it back to the calling function (i.e.: in case we
+    # want to assign a player to that tournament)
     sql = 'INSERT INTO tournaments (name) VALUES (%s) RETURNING id'
-    data = (t_name,)
+    data = (t_name,)  # prevents SQL injection
+
     with get_cursor() as cursor:
         cursor.execute(sql, data)
         tournament_id = cursor.fetchone()[0]
+
     return tournament_id
 
 
@@ -84,9 +95,13 @@ def deleteTournament(tournament=0):
     """
     sql = 'DELETE FROM tournaments'
     data = ('',)
+
     if (type(tournament) is int and tournament != 0):
+        # if function is called with a tournament specified, only delete
+        # that tournament; otherwise, delete all tournaments
         sql += ' WHERE id = %s'
-        data = (tournament,)
+        data = (tournament,)  # prevents SQL injection
+
     with get_cursor() as cursor:
         cursor.execute(sql, data)
 
@@ -101,14 +116,20 @@ def countPlayers(tournament=None):
       tournament: ID of the tournament for which to count players (optional)
     """
     data = ('',)
+
     if (tournament is not None):
+        # if function is called with a tournament specified, only count the
+        # players in that tournament
         sql = 'SELECT COUNT(*) FROM players WHERE tournament_id = %s'
-        data = (tournament,)
+        data = (tournament,)  # prevents SQL injection
     else:
+        # otherwise, return a count of all players in all tournaments
         sql = 'SELECT COUNT(*) FROM players'
+
     with get_cursor() as cursor:
         cursor.execute(sql, data)
         player_count = cursor.fetchone()[0]
+
     return player_count
 
 
@@ -125,50 +146,65 @@ def registerPlayer(p_name, tournament=None):
       name: the player's full name (need not be unique).
       tournament: the ID of the tournament to register the player in (optional)
     """
+    # use "RETURNING" to make sure to return the registrant ID so
+    # we can pass it back to the calling function (i.e.: in case we
+    # want to assign that registrant to a tournament)
     sql = 'INSERT INTO registrants (name) VALUES (%s) RETURNING *'
-    data = (p_name,)
+    data = (p_name,)  # prevents SQL injection
+
     with get_cursor() as cursor:
         cursor.execute(sql, data)
+        # put the new registrant data into new_player
         new_player = cursor.fetchone()[0]
+
     if (tournament and type(tournament) is int):
+        # if tournament is specified, user wants this registrant to be added
+        # as a player to a tournament; so, do that
         sql = 'INSERT INTO players (tournament_id, registrant_id) VALUES '
         sql += '(%s, %s)'
-        data = (tournament, new_player,)
+        data = (tournament, new_player,)  # prevents SQL injection
         with get_cursor() as cursor:
             cursor.execute(sql, data)
 
 
-def assignPlayer(player, tournament):
-    """Assigns a player to a tournament
+def assignPlayer(registrant, tournament):
+    """Assigns a registrant to a tournament
 
     Only used if the player was not associated with a tournament when s/he
     was registered; also only used for existing players.
 
     Args:
-      player: ID of the player in the players table
+      registrant: ID of the player in the players table
       tournament: ID of the tournament
     """
-    if (type(player) is not int or type(tournament) is not int):
-        return 'Either the player or the tournament you entered is invalid.'
-    sql = 'INSERT INTO tournament_players (tournament_id, player_id) '
-    sql += 'VALUES (%s, %s)'
-    data = (tournament, player,)
+    # check function inputs to make sure they're of the right data type
+    if (type(registrant) is not int or type(tournament) is not int):
+        return '''
+        Either the registrant or the tournament you entered is invalid.
+        '''
+
+    sql = 'INSERT INTO players (tournament_id, registrant_id) VALUES (%s, %s)'
+    data = (tournament, registrant,)  # prevents SQL injection
     with get_cursor() as cursor:
         cursor.execute(sql, data)
 
 
-def unAssignPlayer(player, tournament):
-    """Removes a player from a tournament
+def unAssignPlayer(registrant, tournament):
+    """Removes a registrant from a tournament
 
     Args:
-      player: ID of the player in the players table
+      registrant: ID of the player in the players table
       tournament: ID of the tournament
     """
-    if (type(player) is not int or type(tournament) is not int):
-        return 'Either the player or the tournament you entered is invalid.'
-    sql = 'DELETE FROM tournament_players WHERE tournament_id = %s AND '
-    sql += 'player_id = %s'
-    data = (tournament, player,)
+    # check function inputs to make sure they're of the right data type
+    if (type(registrant) is not int or type(tournament) is not int):
+        return 'Either the registrant or tournament you entered is invalid.'
+
+    # note that registrant is not deleted, so can be assigned to other
+    # tournaments
+    sql = 'DELETE FROM players WHERE tournament_id = %s AND player_id = %s'
+    data = (tournament, registrant,)  # prevents SQL injection
+
     with get_cursor() as cursor:
         cursor.execute(sql, data)
 
@@ -190,18 +226,25 @@ def playerStandings(tournament):
         matches: the number of matches the player has played
         omw: the number of wins of all the player's previous opponents
     """
+    # check function input to make sure it's of the right data type
     if (type(tournament) is not int):
         return 'Tournament is invalid (must be a number).'
 
+    # execute the view player_standings to generate the standings list
     sql = 'SELECT player_id, player_name, count_wins, count_matches, omw FROM '
     sql += 'player_standings WHERE tournament_id = %s'
-    data = (tournament,)
+    data = (tournament,)  # prevents SQL injection
+
     with get_cursor() as cursor:
         cursor.execute(sql, data)
         players = cursor.fetchall()
+
     player_list = []
+
+    # build the list of tuples to pass back to the calling function
     for player in players:
         player_list.append(player)
+
     return player_list
 
 
@@ -211,7 +254,11 @@ def reportMatch(winner, loser, tournament, is_tie=False):
     In the case of a tie, "is_tie" will be True and neither of the
     two players will record a win for the match.
 
-    If loser is 0, this is a bye.
+    If loser is 0, this is a bye. Byes occur when there is an odd number
+    of players, meaning that one player in each round will not have a
+    partner. A bye counts as a win, and for that reason no player should
+    receive a bye more than once in a tournament. The system prevents that
+    from happening.
 
     Args:
       winner: the id number of the tournament_player who won
@@ -219,6 +266,7 @@ def reportMatch(winner, loser, tournament, is_tie=False):
       tournament: the ID of the tournament this match was played in
       is_tie: if the match results in a tie, this value is True
     """
+    # check function inputs to make sure they're of the right data type
     if (type(winner) is not int):
         return 'Winner is invalid (must be a number).'
     elif (type(loser) is not int):
@@ -231,13 +279,18 @@ def reportMatch(winner, loser, tournament, is_tie=False):
         return err_msg
 
     if (loser != 0):
+        # if a loser player ID is passed to the function, this is a normal
+        # match; record it in the database
         sql = 'INSERT INTO matches (winner, loser, tournament_id, is_tie) '
         sql += 'VALUES (%s, %s, %s, %s)'
-        data = (winner, loser, tournament, is_tie,)
+        data = (winner, loser, tournament, is_tie,)  # prevents SQL injection
     else:
+        # if 0 is passed to the function as the loser, this is a bye match;
+        # record it as such in the database (see docstring for definition
+        # of a bye)
         sql = 'INSERT INTO matches (winner, tournament_id, is_bye) VALUES '
         sql += '(%s, %s, True)'
-        data = (winner, tournament,)
+        data = (winner, tournament,)  # prevents SQL injection
 
     with get_cursor() as cursor:
         cursor.execute(sql, data)
@@ -268,14 +321,24 @@ def swissPairings(tournament):
         id2: the second tournament_player's unique id
         name2: the second player's name
     """
+    # check function input to make sure it's of the right data type
     if (type(tournament) is not int):
         return 'Tournament is invalid (must be a number).'
+
+    # call the function swiss_pairings(), passing in the tournament ID
+    # swiss_pairings() checks to see which players have already been matched
+    # up and gives us a new round of pairings
     sql = 'SELECT * FROM swiss_pairings(%s)'
-    data = (tournament,)
+    data = (tournament,)  # prevents SQL injection
+
     with get_cursor() as cursor:
         cursor.execute(sql, data)
         pairs = cursor.fetchall()
+
     pair_list = []
+
+    # build the list of tuples to return to the calling function
     for pair in pairs:
         pair_list.append(pair)
+
     return pair_list
